a8d01f9645176d6c02b843dd14658fb23d764911
hamaog <35367953+hamaog@users.noreply.github.com>
2020-03-24 11:24:52 +0900

    Create ch13.py

---
+++ ch13.py
@@ -0,0 +1,23 @@
+#13. col1.txtとcol2.txtをマージ
+#12で作ったcol1.txtとcol2.txtを結合し，元のファイルの1列目と2列目をタブ区切りで並べたテキストファイルを作成せよ．確認にはpasteコマンドを用いよ．
+
+def file_merge(bef_file1: str,bef_file2: str,aft_file: str,sepValue: str):
+    with open(bef_file1) as f:
+        data1 = f.readlines()
+
+    with open(bef_file2) as f:
+        data2 = f.readlines()
+
+    data_merge = '\n'.join(['\t'.join([data11.strip(),data21.strip()]) for data11,data21 in zip(data1,data2)])
+
+    with open(aft_file,'w') as f:
+        f.write(data_merge)
+
+bef_file1 = '/Users/***/col1.txt'
+bef_file2 = '/Users/***/col2.txt'
+aft_file =  '/Users/***/col_merge.txt'
+
+file_merge(bef_file1,bef_file2,aft_file,'\t')
+
+##UNIXコマンド
+##paste col1.txt col2.txt
