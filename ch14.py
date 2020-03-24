6513b3dc514f353f641ecc35506d611c5146ebb1
hamaog <35367953+hamaog@users.noreply.github.com>
2020-03-24 11:40:38 +0900

    Create ch14.py

---
+++ ch14.py
@@ -0,0 +1,16 @@
+#14. 先頭からN行を出力
+#自然数Nをコマンドライン引数などの手段で受け取り，入力のうち先頭のN行だけを表示せよ．確認にはheadコマンドを用いよ．
+
+def file_ContGet(fileName: str,rowNum: int):
+    with open(fileName) as f:
+        fileData = f.readlines()
+
+    return fileData[0:rowNum]
+
+directoryName = '/Users/***/hightemp.txt'
+rowNum = 9
+
+file_ContGet(directoryName,9)
+
+##UNIXコマンド
+##head -n 9 hightemp.txt
